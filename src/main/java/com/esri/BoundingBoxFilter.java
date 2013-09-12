4691a24e181ca3603e189c479c5749fac559d123
mraad@esri.com <mraad@esri.com>
2013-09-12 12:19:06 -0400

    BoundingBoxFilter to use nio ByteBuffer

--- src/main/java/com/esri/BoundingBoxFilter.java
+++ src/main/java/com/esri/BoundingBoxFilter.java
@@ -3,11 +3,10 @@ package com.esri;
 import ch.hsr.geohash.BoundingBox;
 import org.apache.hadoop.hbase.filter.FilterBase;
 
-import java.io.ByteArrayInputStream;
 import java.io.DataInput;
-import java.io.DataInputStream;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * HBase filter used during scan to exclude record with points not in supplied bbox
@@ -38,38 +37,30 @@ public final class BoundingBoxFilter extends FilterBase
             final int offset,
             final int length)
     {
-        boolean filter;
-        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes, offset, length);
-        final DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
-        try
-        {
-            final long bits = dataInputStream.readLong();
-            final double lon = dataInputStream.readDouble();
-            final double lat = dataInputStream.readDouble();
-            if (lon < m_boundingBox.getMinLon())
-            {
-                filter = true;
-            }
-            else if (lon > m_boundingBox.getMaxLon())
-            {
-                filter = true;
-            }
-            else if (lat < m_boundingBox.getMinLat())
-            {
-                filter = true;
-            }
-            else if (lat > m_boundingBox.getMaxLat())
-            {
-                filter = true;
-            }
-            else
-            {
-                filter = false;
-            }
-        }
-        catch (IOException e)
+        final boolean filter;
+        final ByteBuffer byteBuffer = ByteBuffer.wrap(bytes, offset, length);
+        final long bits = byteBuffer.getLong();
+        final double lon = byteBuffer.getDouble();
+        final double lat = byteBuffer.getDouble();
+        if (lon < m_boundingBox.getMinLon())
         {
             filter = true;
+        }
+        else if (lon > m_boundingBox.getMaxLon())
+        {
+            filter = true;
+        }
+        else if (lat < m_boundingBox.getMinLat())
+        {
+            filter = true;
+        }
+        else if (lat > m_boundingBox.getMaxLat())
+        {
+            filter = true;
+        }
+        else
+        {
+            filter = false;
         }
         return filter;
     }
